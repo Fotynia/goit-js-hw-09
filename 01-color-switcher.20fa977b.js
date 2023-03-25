986b3e5a36df84e1fe07d2eddc791bcc214fa52c
Fotynia <114238811+Fotynia@users.noreply.github.com>
2023-03-25 16:43:50 +0000

    Deploying to gh-pages from @ Fotynia/goit-js-hw-09@b89e13e0d48627d240f8fa4189edb5b859e78ee8 🚀

--- 01-color-switcher.20fa977b.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello Word!");
-//# sourceMappingURL=01-color-switcher.20fa977b.js.map
--- 01-color-switcher.42940399.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello Word!");
-//# sourceMappingURL=01-color-switcher.42940399.js.map
